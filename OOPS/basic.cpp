0e02ef2e96eae69d45d16391defbb8ad1cc2acc3
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-01-03 00:20:33 +0530

    starting with oops

--- OOPS/basic.cpp
+++ OOPS/basic.cpp
@@ -1,2 +1,79 @@
 #include<iostream>
 using namespace std;
+
+class Student {
+    string name;
+    float cgpa;
+public:
+    void getPercentage() {
+        cout << (cgpa * 10) << "%" << endl;
+    }
+    
+    // Setters 
+    void setName(string nameVal) {
+        name = nameVal;
+    }
+
+    void setCGPA(float cgpaVal) {
+        cgpa = cgpaVal;
+    }
+
+    // Getters 
+    string getName() {
+        return name;
+    }
+
+    float getCGPA() {
+        return cgpa;
+    }
+};
+
+class User {
+    int id;
+    string username;
+    string password;
+    string bio;
+
+    void deactivate() {
+        cout << "Deleting account" << endl;
+    } 
+
+    void editBio(string newBio) {
+        bio = newBio;
+    }
+};
+
+class Car {
+    string name;
+    string color;
+public:
+    Car() {
+        cout << "This is non-parameterized constructor" << endl;
+    }
+
+    Car(string name, string color) {
+        cout << "This is parameterized constructor" << endl;
+        this -> name = name;
+        this -> color = color;
+    }
+    void start() {
+        cout << "Car is starting..." << endl;
+    }
+    void stop() {
+        cout << "The car is stopping..." << endl;
+    }
+    void getName() {
+        cout << "Car name: " << name << endl;
+    }
+    void getColor() {
+        cout << "Car color: " << color << endl;
+    }
+};
+
+int main() {
+    Student s1;         // Object
+    Car c0;
+    Car c1("Mercedes Benz", "white");
+    c1.getName();
+    c1.getColor();
+}
